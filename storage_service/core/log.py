b3c19cca1750bbfb4e9302b7a9d44e262096f98c
maksi.barannikov <maksi.barannikov@x5.ru>
2023-08-07 01:01:33 +0300

    added logging

---
+++ storage_service/core/log.py
@@ -0,0 +1,73 @@
+import logging
+import logging.config
+
+import structlog
+
+from storage_service.core.settings import env
+
+LOG_LEVEL_ROOT = env.str('LOG_LEVEL_ROOT', default='INFO')
+
+
+LOG_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'text': {
+            '()': structlog.stdlib.ProcessorFormatter,
+            'processor': structlog.dev.ConsoleRenderer(),
+        },
+        'json': {
+            '()': structlog.stdlib.ProcessorFormatter,
+            'processor': structlog.processors.JSONRenderer(ensure_ascii=False),
+            'foreign_pre_chain': [
+                structlog.contextvars.merge_contextvars,
+                structlog.processors.TimeStamper(fmt='iso'),
+                structlog.stdlib.add_logger_name,
+                structlog.stdlib.add_log_level,
+                structlog.stdlib.PositionalArgumentsFormatter(),
+            ],
+        },
+    },
+    'handlers': {
+        'console_text': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'text',
+        },
+        'console_json': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'json',
+        },
+    },
+    'loggers': {
+        'root': {
+            'handlers': ['console_json'],
+            'level': LOG_LEVEL_ROOT,
+            'propagate': False,
+        },
+    },
+}
+
+
+STRUCTLOG_CONFIG = {
+    'context_class': structlog.threadlocal.wrap_dict(dict),
+    'logger_factory': structlog.stdlib.LoggerFactory(),
+    'wrapper_class': structlog.stdlib.BoundLogger,
+    'cache_logger_on_first_use': False,
+    'processors': [
+        structlog.threadlocal.merge_threadlocal,
+        structlog.stdlib.filter_by_level,
+        structlog.processors.TimeStamper(fmt='iso'),
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.UnicodeDecoder(),
+        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+    ],
+}
+
+
+def setup_logging():
+    logging.config.dictConfig(LOG_CONFIG)
+    structlog.configure(**STRUCTLOG_CONFIG)
--- storage_service/main.py
+++ storage_service/main.py
@@ -1,17 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
+from structlog import getLogger
 
 from storage_service.api import endpoints
+from storage_service.core.log import setup_logging
 from storage_service.core.settings import (
     SERVER_HOST,
     SERVER_PORT,
     SERVER_RELOAD,
 )
 
+setup_logging()
+logger = getLogger(__name__)
+
+
 app = FastAPI()
 app.include_router(endpoints.router)
 
 
 def start():
-    """Launched with `poetry run start` at root level"""
-    uvicorn.run("storage_service.main:app", host=SERVER_HOST, port=SERVER_PORT, reload=SERVER_RELOAD)
+    logger.info('Launching the application')
+    uvicorn.run('storage_service.main:app', host=SERVER_HOST, port=SERVER_PORT, reload=SERVER_RELOAD)
