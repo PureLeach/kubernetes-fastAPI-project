905149322e6c846fb9e56a90792fdfae8bd69128
maksi.barannikov <maksi.barannikov@x5.ru>
2023-08-08 22:03:36 +0300

    added test_restoring_objects_from_file

--- storage_service/tests/test_services/conftest.py
+++ storage_service/tests/test_services/conftest.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import pytest
@@ -10,19 +11,35 @@ async def create_objects():
     """Фикстура для записи данных в ОЗУ"""
 
     key = 'test_object_key'
-    value = {'test_object_one': 'payload'}
+    object_data = {'test_object_one': 'payload'}
     expires = 100
-    await cache.set(key, value, ttl=expires)
+    await cache.set(key, object_data, ttl=expires)
     cache_meta[key] = expires
-    return key, value, expires
+    return key, object_data, expires
+
+
+@pytest.fixture()
+async def create_file():
+    """Фикстура создания файла"""
+
+    key = 'test_object_key'
+    object_data = {'test_object_one': 'payload'}
+    expires = 100
+
+    file_content = {key: {'object': object_data, 'ttl': expires}}
+    with Path(OBJECTS_DATA).open('w') as file:
+        json.dump(file_content, file)
+    return key, object_data, expires
 
 
 @pytest.fixture(autouse=True)
 def cleanup_file():
     """
-    Удаление файла по завершении тестирования
+    Удаление файла по завершении тестирования, если он существует
     Запускается после завершения каждого теста в рамках каталога
     """
+    path = Path(OBJECTS_DATA)
 
     yield
-    Path(OBJECTS_DATA).unlink()  # Возвращаем путь к файлу как результат фикстуры
+    if path.exists():
+        path.unlink()
--- storage_service/tests/test_services/test_file_handlers.py
+++ storage_service/tests/test_services/test_file_handlers.py
@@ -4,9 +4,14 @@ from pathlib import Path
 import pytest
 
 from storage_service.services.file_handlers import (
+    restoring_objects_from_file,
     save_objects_to_file,
 )
-from storage_service.settings.core import OBJECTS_DATA
+from storage_service.settings.core import (
+    OBJECTS_DATA,
+    cache,
+    cache_meta,
+)
 
 
 @pytest.mark.asyncio
@@ -24,10 +29,23 @@ async def test_save_objects_to_file(create_objects):
 async def test_checking_contents_of_the_file(create_objects):
     """Проверяем содержание файла"""
 
-    key, value, expires = await create_objects
+    key, object_data, expires = await create_objects
 
     await save_objects_to_file()
     with Path(OBJECTS_DATA).open('r') as file:
-        object_data = json.load(file)
+        json_data = json.load(file)
 
-    assert object_data == {key: {'object': value, 'ttl': expires}}
+    assert json_data == {key: {'object': object_data, 'ttl': expires}}
+
+
+@pytest.mark.asyncio
+async def test_restoring_objects_from_file(create_file):
+    """Проверяем, что данные из файла восстанавливаются в ОЗУ"""
+
+    key, object_data, expires = await create_file
+
+    await restoring_objects_from_file()
+    object_in_ram_cache = await cache.get(key)
+
+    assert cache_meta == {key: expires}
+    assert object_in_ram_cache == object_data
